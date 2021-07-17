355405286a17087ab8278e14335c52ad297b7ea6
LucianaReynaud <reynaud.luciana@gmail.com>
2021-07-17 14:51:15 -0300

    Clean code stock limit

---
+++ stock-limit.py
@@ -0,0 +1,25 @@
+# Choice A unnecessarily included the word stock everywhere.
+# We can assume we are dealing with stocks.
+
+stock_limit_prices = {'LUX': 62.48, 'AAPL': 127.67, 'NVDA': 161.24}
+for stock_ticker, stock_limit_pricein buy_prices.items():
+if stock_limit_price <= get_current_stock_price(ticker):
+        buy_stock(ticker)
+else:
+        watchlist_stock(ticker)
+
+# Choice B could have differentiated limit prices from current price.
+
+prices = {'LUX': 62.48, 'AAPL': 127.67, 'NVDA': 161.24}
+for ticker, pricein prices.items():
+if price <= current_price(ticker):
+        buy(ticker)
+else:
+        watchlist(ticker)
+# Choice C passed.
+limit_prices = {'LUX': 62.48, 'AAPL': 127.67, 'NVDA': 161.24}
+for ticker, limitin limit_prices.items():
+if limit <= get_current_price(ticker):
+        buy(ticker)
+else:
+        watchlist(ticker)
